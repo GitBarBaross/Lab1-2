7a940ec228aff7d49771af31a7d2589bec06151f
GitBarBaross <BarBaross1998@gmail.com>
2019-01-04 01:20:26 +0200

    with AddLong but not correct

--- Laba1-2/Program.cs
+++ Laba1-2/Program.cs
@@ -8,19 +8,21 @@ namespace Laba1_2 {
     class Program {
         static void Main(string[] args) {
 
-            string a = "16D4C";
-            string b = "16D4CA8";
+            string a = "5D13F5BFA0B1EF77EB53FCD7A258EA2D0BFBB3A36DAFE39BC966FC05EC3A8E7B";
+            string b = "C06EC1528543622E8E73155431AC20C77DDB2F0FF53B0FD364367D131579763E";
             a = Add0(a);
             b = Add0(b);
-
-            Console.Write(a + "\n");
+          
             var a_mass = new ulong[a.Length / 8];
             var b_mass = new ulong[b.Length / 8];
             a_mass = ToArr(a, a_mass);
             b_mass = ToArr(b, b_mass);
-            string b1 = ToStr(b_mass);
-            Console.WriteLine(b_mass);
-            Console.WriteLine(b1);
+            var sum = new ulong[0];
+            sum = AddLong(a_mass, b_mass);
+            string summ = ToStr(sum);
+            Console.WriteLine(summ);
+            
+           
             Console.ReadKey();
 
 
@@ -34,25 +36,28 @@ namespace Laba1_2 {
             return a;
         }
 
+      
+
+
 
         public static string Del0(string h) {
-
+            h = h.TrimStart('0');
             
-
+            
 
             return h;
         }
 
-        public static ulong[] ToArr(string c, ulong[] a) {
+        public static ulong[] ToArr(string a, ulong[] a_mass) {
 
-            var a32 = new ulong[c.Length / 8];
-            for (int i = 0; i < c.Length; i += 8) {
-                a32[i / 8] = Convert.ToUInt64(c.Substring(i, 8), 16);
-                a[i / 8] = a32[i / 8];
+            var a32 = new ulong[a.Length / 8];
+            for (int i = 0; i < a.Length; i += 8) {
+                a32[i / 8] = Convert.ToUInt64(a.Substring(i, 8), 16);
+                a_mass[i / 8] = a32[i / 8];
                 Array.Reverse(a32);
-                Array.Reverse(a);
+                Array.Reverse(a_mass);
             }
-            return a;
+            return a_mass;
         }
 
 
@@ -62,13 +67,30 @@ namespace Laba1_2 {
             for (int i = 0; i < b.Length; i++) {
                 x = (b[i].ToString("X").PadLeft(8, '0')) + x;
             }
-         
+            x = x.TrimStart('0');
             return x;
 
 
         }
 
+        public static ulong[] AddLong(ulong[] a, ulong[] b) {
+            ulong carry = 0;
+            int max = 0;
+            if (a.Length > b.Length) {  max = a.Length; } 
+            else {  max = b.Length; };
+            Array.Resize(ref a, max);
+            Array.Resize(ref b, max);
+            var ot = new ulong[max + 1];
+            for (int i=0; i<max; i++) {
+                ulong p = a[i] + b[i] + carry;
+                carry = p >> 32;
+                ot[i] = p & 0xffffffff;
+                
+            }
 
+            return ot;
+
+        }
 
 
 
